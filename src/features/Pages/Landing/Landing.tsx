c0abf9d99e3595f1493eec87cc1a37560ce2412e
Michael Whitt <michaelwhitt90@gmail.com>
2023-01-05 01:19:16 -0600

    feat(landing): add flair and links to landing page

--- src/features/Pages/Landing/Landing.tsx
+++ src/features/Pages/Landing/Landing.tsx
@@ -1,5 +1,9 @@
 import generateEdgeBtnTitle from '../../../utils/generateEdgeBtnTitle'
 import {useState, useEffect} from 'react'
+import emailIcon from '../../../assets/email.png'
+import githubIcon from '../../../assets/github.png'
+import linkedIcon from '../../../assets/linked.png'
+import contactIcon from '../../../assets/contact.png'
 
 const Landing = (props) => {
     const paragraphText = `I am a software engineer who has worked on projects of all sizes. I thrive in environments
@@ -29,12 +33,26 @@ const Landing = (props) => {
                 }}
             > 
                 <div id='landing-content'>
-                        <h1>Hello, my name is</h1>
-                        <h2 style={{fontSize: 50, marginTop: -10}}>MICHAEL WHITT.</h2>
+                    
+                        <span style={{height: 100, width: 2, borderLeft: '1px solid #a0d6b4', position: 'absolute', left: '2%', top: 25}}/>
+                        <span style={{height: 1, width: 100, borderTop: '1px solid #a0d6b4', position: 'absolute', left: '2%', top: 25}}/>
+                   
+                        <h1 style={{color: '#a0d6b4', fontSize: 16}}>Hello, my name is</h1>
+                        <h2 style={{fontSize: 50, marginTop: -10, color: '#a0d6b4'}}>MICHAEL WHITT.</h2>
                         <h3 style={{fontSize: 40, marginTop: -10, color: '#ff0011'}}>I build web and mobile apps.</h3>
-                        <p style={{fontSize: 30, marginTop: -10, color: '#ccc', width: '70%', fontWeight: 600}}>
+                        <p style={{fontSize: 30, marginTop: -10, color: '#aaa', width: '70%', fontWeight: 600}}>
                             {text}{text.length !== paragraphText.length ? '_' : ''}
                         </p>
+                        <div style={{width: 300, height: 40, borderBottom: '1px solid #a0d6b4', position: 'absolute', right: '15%', bottom: 30}}>
+                            <div style={{display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: 20, paddingLeft: 20}}>
+                                <img src={githubIcon} width={30} className='navIcon' />
+                                <img src={linkedIcon} width={30} className='navIcon' />
+                                <img src={emailIcon} width={30} className='navIcon' />
+                                <img src={contactIcon} width={30} className='navIcon' />
+                            </div>
+
+                        </div>
+                        
                 </div>
                 <div 
                     className='edgeContainer' 
