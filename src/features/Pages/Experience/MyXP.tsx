a7d3112b04942a21361ad5cee9722bbe6ad5d3f4
Michael Whitt <michaelwhitt90@gmail.com>
2023-01-07 21:34:05 -0600

    feat(xp): add xp container and list items

--- src/features/Pages/Experience/MyXP.tsx
+++ src/features/Pages/Experience/MyXP.tsx
@@ -1,8 +1,42 @@
+import {useState} from 'react'
 const MyXP = () => {
-    
-    return(
-        <div className='my-xp'>
+    const [chosen, setChosen] = useState('ClearC2')
 
+    const work = ['ClearC2', 'Astraphos', 'Dreamland', 'Mosaic']
+    const school = ['NC', 'UNNC', 'UNL', 'PKU']
+    return(
+        <div className='xp-container'>
+            <div className='xp-sidebar'>
+                <div className='center xp-title' style={{borderRadius: '12px 0 0 0 '}}>
+                    Work
+                </div>
+                <div className='xp-list'>
+                    {work.map(item => (
+                        <div 
+                            key={item} 
+                            className={`center xp-item${item === chosen ? ' active' : ''}`}
+                            onClick={() => setChosen(item)}
+                        >
+                            {item}
+                        </div>
+                    ))}
+                </div>
+                <div className='center xp-title'>
+                    Education
+                </div>
+                <div className='xp-list'>
+                    {school.map(item => (
+                        <div 
+                            key={item} 
+                            className={`center xp-item${item === chosen ? ' active' : ''}`}
+                            onClick={() => setChosen(item)}
+                            style={{borderRadius: item === school.slice(-1)[0] ? '0 0 0 12px' : ''}}
+                        >
+                            {item}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
