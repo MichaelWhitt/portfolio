52b191579c3c38ffc9812655adf01df9d267b625
Michael Whitt <michaelwhitt90@gmail.com>
2023-01-10 17:27:48 -0600

    feat(xp): add xp icons and animation

--- src/features/Pages/Experience/XPContainer.tsx
+++ src/features/Pages/Experience/XPContainer.tsx
@@ -2,13 +2,16 @@ import {useState, useContext} from 'react'
 import { PageAnimationContext } from '../Stacker'
 import Scrollbar from './Scrollbar'
 import XPTile from './XPTile'
+import Animate from '../../AnimateWrapper'
+import grad from '../../../assets/grad.png'
+import workspace from '../../../assets/workspace.png'
 
 const XPContainer = () => {
     const [chosen, setChosen] = useState('ClearC2')
 
     const work = ['ClearC2', 'NuCamp', 'Astraphos', 'Dreamland']
     const school = ['NC', 'UNNC', 'UNL', 'PKU']
-    const context = useContext(PageAnimationContext)
+    const {visitedContext} = useContext(PageAnimationContext)
 
     return(
         <div style={{height: '100%', paddingTop: 200, width: '100%'}}>
@@ -16,7 +19,11 @@ const XPContainer = () => {
                 <Scrollbar chosenIndex={[0, ...work, 5, ...school].indexOf(chosen)} />
                 <div className='xp-sidebar'>
                     <div className='center xp-title'>
-                        Work
+                        {visitedContext.second && (
+                            <Animate from={{x: -250}} to={{x: 0}} delay={300}>
+                                <img src={workspace} width={50} height={50}/>
+                            </Animate>
+                        )}
                     </div>
                     <div className='xp-list'>
                         {work.map(item => (
@@ -30,7 +37,11 @@ const XPContainer = () => {
                         ))}
                     </div>
                     <div className='center xp-title'>
-                        Education
+                        {visitedContext.second && (
+                            <Animate from={{x: -250}} to={{x: 0}} delay={300}>
+                                <img src={grad} width={50} height={50}/>
+                            </Animate>
+                        )}
                     </div>
                     <div className='xp-list'>
                         {school.map(item => (
