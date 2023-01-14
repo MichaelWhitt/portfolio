2831439f57373fa56a21650d1d92e25a2ed328e4
Michael Whitt <michaelwhitt90@gmail.com>
2023-01-14 12:47:02 -0600

    fix(proj): test mp4 methods

--- src/features/Pages/Projects/ProjectTile.tsx
+++ src/features/Pages/Projects/ProjectTile.tsx
@@ -9,7 +9,7 @@ import bootstrapLogo from '../../../assets/bootstraplogo.png'
 import reactspringLogo from '../../../assets/reactspringlogo.png'
 import githubLogo from '../../../assets/github.png'
 import eyeLogo from '../../../assets/eye.png'
-// import showTVid from '/Development/portfolio2023/src/assets/showvid.mp4'
+import showTVid from '/Development/portfolio2023/src/assets/showvid.mp4'
 import raidVid from '../../../assets/raidVid.mp4'
 import portVid from '../../../assets/portVid.mp4'
 import cwlVid from '../../../assets/canwelistenVid.mp4'
@@ -30,7 +30,7 @@ const ProjectTile = (props) => {
             viewLink = 'https://michaeldwhitt.com'
             break
         case 'Show Tracker': 
-            src = portVid
+            src = showTVid
             tools = ['react', 'redux', 'fauna', 'netlify', 'javascript']
             githubLink = 'https://github.com/MichaelWhitt/show-tracker'
             viewLink = 'https://mwmovies.netlify.app/'
@@ -148,9 +148,10 @@ const ProjectTile = (props) => {
                         <video 
                             width={'100%'} 
                             height={'100%'} 
-                            src={src} 
-                            onMouseOver={e => e.currentTarget.play()}
-                            onMouseOut={e => e.currentTarget.pause()}
+                            src={src}
+                            style={{borderRadius: 12}}
+                            // onMouseOver={e => e.currentTarget.play()}
+                            // onMouseOut={e => e.currentTarget.pause()}
                         />
                     </div>
                 </div>
